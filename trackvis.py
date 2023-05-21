453763b4692368467a0ed361ea8879f86ebedec3
Martin Craig <martin.craig@nottingham.ac.uk>
2023-05-21 17:38:35 +0100

    WIP commit

--- trackvis.py
+++ trackvis.py
@@ -102,11 +102,14 @@ class OrthoView:
         )
         self._bgvis.transform = scene.transforms.MatrixTransform(transforms.rotate(90, (1, 0, 0)))
 
-    def add_tractogram(self, tractogram, name, cmap="viridis", radius=0.5):
-        cmap = colormap.get_colormap(cmap)
-        densities = tractogram.data_per_point['densities']
-        densities_norm = [(d - np.min(d)) / (np.max(d) - np.min(d)) for d in densities]
-        all_vertex_colors = [cmap.map(d) for d in densities_norm]
+    def set_tractogram(self, name, streamlines, color="red", vertex_data=None, cmap="viridis", radius=0.5):
+        streamlines = streamlines[::20]
+        if vertex_data is not None:
+            vertex_data = vertex_data[::20]
+            cmap = colormap.get_colormap(cmap)
+            vertex_data_norm = [(d - np.min(d)) / (np.max(d) - np.min(d)) for d in vertex_data]
+            vertex_colors = [cmap.map(d) for d in vertex_data_norm]
+
         w2v = np.linalg.inv(self._affine)
         i = np.identity(4)
         v2s = np.identity(4)
@@ -124,16 +127,30 @@ class OrthoView:
         print(self._flip)
         print(self.zpos)
 
-        self.remove_tractogram(name)
-        visuals = []
-        for sldata, vertex_colors in zip(tractogram.streamlines[::10], all_vertex_colors[::10]):
-            vertex_colors=np.repeat(vertex_colors, 8, axis=0)
-            vis = scene.visuals.Tube(sldata, radius=radius, vertex_colors=vertex_colors, parent=self._scene, shading='smooth')
+        if name not in self._tractograms:
+            visuals = []
+            for idx, sldata in enumerate(streamlines):
+                print(f"Vis: {idx+1}/{len(streamlines)}")
+                #vis = scene.visuals.Tube(sldata, radius=radius, parent=self._scene, shading='smooth')
+                vis = scene.visuals.Line(sldata, parent=self._scene)
+                #vis.shading_filter.light_dir = (1, 1, 0)
+                #vis.shading_filter.ambient_light = (1, 1, 1, 0.5)
+                visuals.append(vis)
+            self._tractograms[name] = visuals
+
+        for idx, vis in enumerate(self._tractograms[name]):
+            print(f"Props: {idx+1}/{len(streamlines)}")
             vis.transform = scene.transforms.MatrixTransform(np.dot(w2s.T, transforms.rotate(90, (1, 0, 0))))
-            vis.shading_filter.light_dir = (0, 1, 0)
-            vis.shading_filter.ambient_light = (1, 1, 1, 0.5)
-            visuals.append(vis)
-        self._tractograms[name] = visuals
+            #md = vis.mesh_data
+            #if vertex_data is not None:
+            #    vis.set_data(vertices=md.get_vertices(), faces=md.get_faces(), vertex_colors=np.repeat(vertex_colors[idx], 8, axis=0))
+            #else:
+            #    vis.set_data(vertices=md.get_vertices(), faces=md.get_faces(), color=color)
+            pos = vis.pos
+            if vertex_data is not None:
+                vis.set_data(pos=pos, color=vertex_colors[idx])
+            else:
+                vis.set_data(pos=pos, color=color)
 
     def remove_tractogram(self, name):
         if name in self._tractograms:
@@ -181,202 +198,102 @@ class VolumeSelection(QWidget):
                 import traceback
                 traceback.print_exc()
 
-class MultiSelectCombo(QtWidgets.QComboBox):
-    """
-    A combo box which allows multiple items to be selected
-    """
-    sig_changed = QtCore.Signal()
+class TractView(QWidget):
 
-    def __init__(self, parent=None, **kwargs):
-        super(MultiSelectCombo, self).__init__(parent)
-        #self.setSizePolicy(QtWidgets.QSizePolicy.Maximum, QtWidgets.QSizePolicy.Maximum)
-        self._changed = False
-        self._all_items = []
+    sig_changed = QtCore.Signal(object)
 
-        self.set_choices([])
+    COLORS = ["red", "green", "blue"]
 
-        self.currentIndexChanged.connect(self._index_changed)
-        self.view().pressed.connect(self._item_pressed)
-        delegate = QtWidgets.QStyledItemDelegate(self.view())
-        self.view().setItemDelegate(delegate)
-
-        # Need to intercept the default resize event
-        # FIXME why can't call superclass method normally?
-        self.resizeEventOrig = self.resizeEvent
-        self.resizeEvent = self._resized
-
-    @property
-    def selected(self):
-        """
-        Get the names of the selected item(s)
-        """
-        ret = []
-        for idx in range(1, self.count()):
-            item = self.model().item(idx, 0)
-            if item.checkState() == QtCore.Qt.Checked:
-                ret.append(self.itemData(idx))
-        return ret
-
-    @selected.setter
-    def selected(self, val):
-        """
-        Set the selected item(s)
-        """
-        for name in val:
-            idx = self.findData(name)
-            item = self.model().item(idx, 0)
-            item.setCheckState(QtCore.Qt.Checked)
-
-    def set_choices(self, items):
-        if items and items == self._all_items:
-            return
-
-        self._all_items = items
-        current = self.selected
-        self.blockSignals(True)
-        try:
-            self.clear()
-
-            text, tooltip = self._list_text(current)
-            self._add(text, tooltip)
-
-            idx = 1
-            for name in sorted(items):
-                self._add(name)
-                item = self.model().item(idx, 0)
-                if name in current:
-                    item.setCheckState(QtCore.Qt.Checked)
-                else:
-                    item.setCheckState(QtCore.Qt.Unchecked)
-                idx += 1
-
-            # Make sure names are visible even with drop down arrow
-            width = self.minimumSizeHint().width()
-            self.setMinimumWidth(width+50)
-        finally:
-            self.blockSignals(False)
-
-        self.setCurrentIndex(0)
-
-    def _item_pressed(self, idx):
-        """ One of the checkboxes has been pressed """
-        item = self.model().itemFromIndex(idx)
-        if item.checkState() == QtCore.Qt.Checked:
-            item.setCheckState(QtCore.Qt.Unchecked)
-        else:
-            item.setCheckState(QtCore.Qt.Checked)
-        selected_items = self.selected
-        text, tooltip = self._list_text(selected_items)
-        self.setItemText(0, text)
-        self.setItemData(0, text)
-        self.setItemData(0, tooltip, QtCore.Qt.ToolTipRole)
-        self.sig_changed.emit()
-        self._changed = True
-
-    def hidePopup(self):
-        """
-        Overridden from QtWidgets.QComboBox
-
-        To allow multi-select, don't hide the popup when it's clicked on to
-        select/deselect items, so we can check and uncheck
-        them in one go. However if nothing has changed as
-        a result of the click (e.g. we click outside the popup
-        window), this will close the popup
-        """
-        if not self._changed:
-            QtWidgets.QComboBox.hidePopup(self)
-        self._changed = False
-
-    def _list_text(self, selected_items):
-        """:return: Text to be used when list not visible"""
-        if selected_items:
-            return "%i items" % len(selected_items), ",".join(selected_items)
-        else:
-            return "<Select items>", ""
-
-    def _index_changed(self, _idx):
-        self.setCurrentIndex(0)
-
-    def _add(self, name, tooltip=None):
-        """
-        Add an item, with shortened display name and full name as data
-        """
-        self.addItem(self._elided_name(name), name)
-        if not tooltip:
-            tooltip = name
-        self.setItemData(self.count()-1, tooltip, QtCore.Qt.ToolTipRole)
-
-    def _elided_name(self, name):
-        """
-        Put ellipsis between truncated name to make it fit in the combo
-        """
-        if name.startswith("<"):
-            # Special name, not a data set name
-            return name
-        width = self.fontMetrics().boundingRect(name).width()
-        combo_width = self.geometry().width() - 70
-        elided_name = name
-        left_chars = len(elided_name)//2
-        right_chars = left_chars-1
-        while width > combo_width:
-            elided_name = elided_name[:left_chars] + "..." + elided_name[-right_chars:]
-            width = self.fontMetrics().boundingRect(elided_name).width()
-            if width < combo_width:
-                break
-            elif left_chars < 2 or right_chars < 2:
-                break
-            if left_chars < right_chars:
-                right_chars -= 1
-            else:
-                left_chars -= 1
-        return elided_name
-
-    def _resized(self, event):
-        self.resizeEventOrig(event)
-        for idx in range(self.count()):
-            data_name = self.itemData(idx)
-            self.setItemText(idx, self._elided_name(data_name))
-
-class TractView:
-
-    def __init__(self, tractogram):
+    def __init__(self, name, tractogram):
+        QWidget.__init__(self)
+        
+        self.name = name
         self.streamlines = tractogram.streamlines
-        self.vertex_data = tractogram.vertex_data
+        self._vertex_data = tractogram.data_per_point
         self._color = "red"
         self._color_by = None
         self._cmap = "viridis"
         self._visuals = []
 
+        hbox = QHBoxLayout()
+        self._select_cb = QtWidgets.QCheckBox()
+        self._select_cb.stateChanged.connect(self._select_changed)
+        hbox.addWidget(self._select_cb)
+        self._name_edit = QtWidgets.QLineEdit()
+        self._name_edit.setText(name)
+        hbox.addWidget(self._name_edit)
+
+        hbox.addWidget(QtWidgets.QLabel("Colour"))
+        self._color_combo = QtWidgets.QComboBox()
+        for col in self.COLORS:
+            self._color_combo.addItem(col)
+        self._color_combo.currentIndexChanged.connect(self._color_changed)
+        hbox.addWidget(self._color_combo)
+
+        hbox.addWidget(QtWidgets.QLabel("Vertex data"))
+        self._data_combo = QtWidgets.QComboBox()
+        self._data_combo.addItem("None")
+        for data_name in self._vertex_data.keys():
+            self._data_combo.addItem(data_name)
+        self._data_combo.currentIndexChanged.connect(self._data_changed)
+        hbox.addWidget(self._data_combo)
+
+        hbox.addWidget(QtWidgets.QLabel("Colour map"))
+        self._cmap_combo = QtWidgets.QComboBox()
+        for cmap_name in colormap.get_colormaps():
+            self._cmap_combo.addItem(cmap_name)
+        self._cmap_combo.currentIndexChanged.connect(self._cmap_changed)
+        hbox.addWidget(self._cmap_combo)
+        
+        self.setLayout(hbox)
+        self._select_changed()
+
+    def _color_changed(self, idx):
+        self._color = self._color_combo.itemText(idx)
+        self.sig_changed.emit(self)
+
+    def _data_changed(self, idx):
+        if idx == 0:
+            self._color_by = None
+        else:
+            self._color_by = self._data_combo.itemText(idx)
+        self._select_changed()
+
+    def _cmap_changed(self, idx):
+        self._cmap = self._cmap_combo.itemText(idx)
+        self.sig_changed.emit(self)
+
+    def _select_changed(self):
+        enabled = self.enabled
+        color_by = self.color_by
+        self._color_combo.setEnabled(enabled and color_by is None)
+        self._cmap_combo.setEnabled(enabled and color_by is not None)
+        self._data_combo.setEnabled(enabled)
+        self.sig_changed.emit(self)
+
     @property
     def vertex_data_items(self):
-        return list(self.vertex_data.keys())
-    
+        return list(self._vertex_data.keys())
+
+    @property
+    def vertex_data(self):
+        return None if self.color_by is None else self._vertex_data[self.color_by]
+
     @property
     def color(self):
-        return self._color
-    
-    @color.setter
-    def set_color(self, color):
-        self._color = color
+        return self._color if self.color_by is None else None
 
     @property
     def color_by(self):
         return self._color_by
-    
-    @color_by.setter
-    def set_color_by(self, color_by):
-        self._color_by = color_by
 
     @property
     def cmap(self):
-        return self._cmap
-    
-    @cmap.setter
-    def set_cmap(self, cmap):
-        self._cmap = cmap
-    
-    def add_visual(self):
-        pass
+        return self._cmap if self.color_by is not None else None
+
+    @property
+    def enabled(self):
+        return self._select_cb.isChecked()
 
 class TractSelection(QWidget):
 
@@ -386,7 +303,8 @@ class TractSelection(QWidget):
         self._tract_dir = ""
         self._selected_tracts = []
 
-        vbox = QVBoxLayout()
+        self._vbox = QVBoxLayout()
+        self._vbox.setSpacing(1)
 
         hbox = QHBoxLayout()
         hbox.addWidget(QtWidgets.QLabel("XTRACT/Probtrackx2 output: "))
@@ -395,47 +313,45 @@ class TractSelection(QWidget):
         self._button = QtWidgets.QPushButton("Choose directory")
         self._button.clicked.connect(self._choose_file)
         hbox.addWidget(self._button)
-        vbox.addLayout(hbox)
+        self._vbox.addLayout(hbox)
 
-        hbox = QHBoxLayout()
-        hbox.addWidget(QtWidgets.QLabel("Visible Tracts "))
-        self._tracts_combo = MultiSelectCombo()
-        self._tracts_combo.sig_changed.connect(self._tracts_changed)
-        hbox.addWidget(self._tracts_combo)
-        hbox.addWidget(QtWidgets.QLabel("Colour"))
-        self._colour_combo = QtWidgets.QComboBox()
-        #self._colour_combo.sig_changed.connect(self._colour_changed)
-        hbox.addWidget(self._colour_combo)
-        hbox.addWidget(QtWidgets.QLabel("Vertex data"))
-        self._data_combo = QtWidgets.QComboBox()
-        #self._data_combo.sig_changed.connect(self._vertex_data_changed)
-        hbox.addWidget(self._data_combo)
-        hbox.addWidget(QtWidgets.QLabel("Colour map"))
-        self._trk_cmap_combo = QtWidgets.QComboBox()
-        #self._trk_cmap_combo.sig_changed.connect(self._trk_cmap_data_changed)
-        hbox.addWidget(self._data_combo)
-        vbox.addLayout(hbox)
+        self.setLayout(self._vbox)
 
-        self.setLayout(vbox)
 
-    def _get_tracts(self, xtract_dir):
+    def _set_xtract_dir(self, xtract_dir):
+        self._edit.setText(xtract_dir)
+        while 1:
+            child = self._vbox.takeAt(1)
+            if not child:
+                break
+            child.widget().deleteLater()
+
         self._tract_dir = os.path.join(xtract_dir, "tracts")
-        tracts = []
         for dname in os.listdir(self._tract_dir):
             if os.path.isdir(os.path.join(self._tract_dir, dname)) and os.path.exists(os.path.join(self._tract_dir, dname, "streamlines.trk")):
-                tracts.append(dname)
-        self._tracts_combo.set_choices(tracts)
-        if tracts:
-            self._tracts_combo.selected = [tracts[0]]
+                trk = nib.streamlines.load(os.path.join(self._tract_dir, dname, "streamlines.trk"))
+                print(f"Creating tract view {dname}")
+                tract_view = TractView(dname, trk.tractogram)
+                tract_view.sig_changed.connect(self._tract_view_changed)
+                self._vbox.addWidget(tract_view)
+        self._vbox.addStretch()
 
     def _choose_file(self):
         xtract_dir = QtWidgets.QFileDialog.getExistingDirectory()
         if xtract_dir:
             for view in self._viewer.views:
                 view.clear_tractograms()
-            self._get_tracts(xtract_dir)
-            self._edit.setText(xtract_dir)
-    
+            
+            self._set_xtract_dir(xtract_dir)
+
+    def _tract_view_changed(self, tv):
+        print("tracts changed: {tv.name}")
+        for view in self._viewer.views:
+            if not tv.enabled:
+                view.remove_tractogram(tv.name)
+            else:
+                view.set_tractogram(tv.name, tv.streamlines, color=tv.color, vertex_data=tv.vertex_data, cmap=tv.cmap)
+
     def _tracts_changed(self):
         selected_now = self._tracts_combo.selected
         print("tracts changed: ", selected_now)
